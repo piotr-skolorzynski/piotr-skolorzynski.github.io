2dbc43b16722a4584bddfe58fb2333c31bf8514f
Piotr <p.skolorzynski@gmail.com>
2021-05-03 23:35:00 +0200

    modify index.html style.css and script.js files handle navigation background depending on scrollY work in progress

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const navbarBtn = document.querySelector('.navbar');
 const navMobile = document.querySelector('.nav-mobile');
+const nav = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav-list');
+const navDesktop = document.querySelectorAll('.nav-desktop');
+const allSections = document.querySelectorAll('.section');
+
+
+const sectionObserver = () => {
+    const currentSection = window.scrollY;
+    allSections.forEach(section => {
+        if (currentSection <= 100) {
+            nav.classList.remove('bcg-1');
+            nav.classList.remove('bcg-2');
+        } else if (currentSection > 100 && currentSection <= 900) {
+            nav.classList.remove('bcg-1');
+            nav.classList.add('bcg-2');
+        } else if (section.classList.contains('about')) {
+            nav.classList.remove('bcg-2');
+            nav.classList.add('bcg-1');
+        }
+        // } else if (section.classList.contains('experience')) {
+        //     nav.classList.remove('bcg-1');
+        //     nav.classList.add('bcg-2');
+        // } else if (section.classList.contains('skills')) {
+        //     nav.classList.remove('bcg-2');
+        //     nav.classList.add('bcg-1');  
+        // } else if (section.classList.contains('hobbies')) {
+        //     nav.classList.remove('bcg-1');
+        //     nav.classList.add('bcg-2');
+        // }
+    });
+};
 
 const listenLinks = () => {
     navLinks.forEach(item => {
@@ -14,4 +44,6 @@ const handleNavigation = () => {
     listenLinks();
 };
 
+document.addEventListener('scroll', sectionObserver);
+// navDesktop.addEventListener('click', linkObserver);
 navbarBtn.addEventListener('click', handleNavigation);
