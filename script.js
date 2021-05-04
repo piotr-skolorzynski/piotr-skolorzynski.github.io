eac1b8647bbed63d1c3e52c03f6cf60e0aa4286f
Piotr <p.skolorzynski@gmail.com>
2021-05-04 10:15:38 +0200

    modify index.html style.css and script.js add function scrollspy to show current section on navigation

--- script.js
+++ script.js
@@ -2,8 +2,44 @@ const navbarBtn = document.querySelector('.navbar');
 const navMobile = document.querySelector('.nav-mobile');
 const nav = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav-list');
-const navDesktop = document.querySelectorAll('.nav-desktop');
+// const navDesktop = document.querySelectorAll('.nav-desktop');
 const allSections = document.querySelectorAll('.section');
+
+let about = document.querySelector('#about');
+let experience = document.querySelector('#experience');
+let skills = document.querySelector('#skills');
+let hobbies = document.querySelector('#hobbies');
+
+// scrollspy
+const scrollspy = () => {
+    const win = window.pageYOffset;
+    if (about.offsetTop <= win && experience.offsetTop > win) {
+        document.querySelector('#link-about').classList.add('active');
+        document.querySelector('#link-experience').classList.remove('active');
+        document.querySelector('#link-skills').classList.remove('active');
+        document.querySelector('#link-hobbies').classList.remove('active');
+    } else if (experience.offsetTop <= win && skills.offsetTop > win) {
+        document.querySelector('#link-about').classList.remove('active');
+        document.querySelector('#link-experience').classList.add('active');
+        document.querySelector('#link-skills').classList.remove('active');
+        document.querySelector('#link-hobbies').classList.remove('active');
+    } else if (skills.offsetTop <= win && hobbies.offsetTop > win) {
+        document.querySelector('#link-about').classList.remove('active');
+        document.querySelector('#link-experience').classList.remove('active');
+        document.querySelector('#link-skills').classList.add('active');
+        document.querySelector('#link-hobbies').classList.remove('active');
+    } else if (hobbies.offsetTop <= win) {
+        document.querySelector('#link-about').classList.remove('active');
+        document.querySelector('#link-experience').classList.remove('active');
+        document.querySelector('#link-skills').classList.remove('active');
+        document.querySelector('#link-hobbies').classList.add('active');
+    } else {
+        document.querySelector('#link-about').classList.remove('active');
+        document.querySelector('#link-experience').classList.remove('active');
+        document.querySelector('#link-skills').classList.remove('active');
+        document.querySelector('#link-hobbies').classList.remove('active');
+    }
+};
 
 // change nav background 
 const handleObserver = () => {
@@ -38,5 +74,6 @@ const handleNavigation = () => {
     handleLinks();
 };
 
+document.addEventListener('scroll', scrollspy);
 document.addEventListener('scroll', handleObserver);
 navbarBtn.addEventListener('click', handleNavigation);
