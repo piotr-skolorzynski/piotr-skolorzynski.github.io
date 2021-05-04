842a4d2d693620d39db12c30d2b84f9d1005cc43
Piotr <p.skolorzynski@gmail.com>
2021-05-04 08:35:09 +0200

    modify index.html style.css and script.js files add feature to handle navigation backgroundColor when changing sections

--- script.js
+++ script.js
@@ -5,45 +5,38 @@ const navLinks = document.querySelectorAll('.nav-list');
 const navDesktop = document.querySelectorAll('.nav-desktop');
 const allSections = document.querySelectorAll('.section');
 
-
-const sectionObserver = () => {
-    const currentSection = window.scrollY;
+// change nav background 
+const handleObserver = () => {
+    const currentPosition = window.scrollY;
     allSections.forEach(section => {
-        if (currentSection <= 100) {
+        if (currentPosition <= 100) {
             nav.classList.remove('bcg-1');
             nav.classList.remove('bcg-2');
-        } else if (currentSection > 100 && currentSection <= 900) {
+        } else if ((currentPosition > 100 && currentPosition <= 900)) {
             nav.classList.remove('bcg-1');
             nav.classList.add('bcg-2');
-        } else if (section.classList.contains('about')) {
+        } else if (section.classList.contains('section-bcg2') && section.offsetTop <= currentPosition + 60) {
+            nav.classList.remove('bcg-1');
+            nav.classList.add('bcg-2');
+        }  else if (section.classList.contains('section-bcg1') && section.offsetTop <= currentPosition + 60) {
             nav.classList.remove('bcg-2');
             nav.classList.add('bcg-1');
-        }
-        // } else if (section.classList.contains('experience')) {
-        //     nav.classList.remove('bcg-1');
-        //     nav.classList.add('bcg-2');
-        // } else if (section.classList.contains('skills')) {
-        //     nav.classList.remove('bcg-2');
-        //     nav.classList.add('bcg-1');  
-        // } else if (section.classList.contains('hobbies')) {
-        //     nav.classList.remove('bcg-1');
-        //     nav.classList.add('bcg-2');
-        // }
+        }        
     });
 };
 
-const listenLinks = () => {
+// close mobile navigation when link is chosen
+const handleLinks = () => {
     navLinks.forEach(item => {
         item.addEventListener('click', handleNavigation);
     });
 };
 
-//zapytaj czy takie rozwiązanie jest dobre czy też lepiej stworzyć oddzielną funkcję do usuwania klasy show po kliknięciu 
+// show or hide mobile navigation if navbar button is clicked
 const handleNavigation = () => {
     navMobile.classList.toggle('show');
-    listenLinks();
+    handleLinks();
 };
 
-document.addEventListener('scroll', sectionObserver);
-// navDesktop.addEventListener('click', linkObserver);
+document.addEventListener('scroll', handleObserver);
 navbarBtn.addEventListener('click', handleNavigation);
